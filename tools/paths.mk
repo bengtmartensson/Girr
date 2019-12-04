f88a0d438eb8ba682969d26a4cc0bc6cc7f06f2b
Bengt Martensson <barf@bengt-martensson.de>
2019-12-04 15:23:19 +0100

    Fixes to pom.xml and Makefile, cf. #15.

--- tools/paths.mk
+++ tools/paths.mk
@@ -1,9 +1,5 @@
 # External programs
 
-INSTALLDIR := /usr/local/share/irptransmogrifier
-BINLINK := /usr/local/bin/irptransmogrifier
-BROWSELINK := /usr/local/bin/irpbrowse
-
 BROWSE := xdg-open
 JAVA := java
 SAXON_JAR := /opt/saxon/saxon9he.jar
