0191c6c0907dfff166b45de156461eb72c5dab91
Bengt Martensson <barf@bengt-martensson.de>
2022-05-24 23:08:08 +0200

    Update of API documentation

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.harctoolbox.girr"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.harctoolbox.girr"}];updateSearchResults();
